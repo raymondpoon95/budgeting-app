c4c76aa9c07dec970ce536bec0f1c76cbf1cdac1
Raymond Poon <raymondpoon95@hotmail.co.uk>
2019-11-13 00:54:02 +0000

    top html

--- app.js
+++ app.js
@@ -5,16 +5,7 @@
  * budget control module, handles the data
  */
 let budgetController = (function(){
-    let x = 23;
-    let add = function(a){          // this cannot but accessed publicly e.g from console
-        return x + a;
-    }
 
-    return {
-        publicTest: function(b){    // returns an object 
-            return add(b);
-        }
-    }
 
 })();
 
@@ -31,15 +22,9 @@ let UIController = (function(){
 
 /************************************************************
  * controller module, handles the link between the UI and budget controller modules
+ * the controller module always tells the other controllers what to do
  */
 let controller = (function(budgetCtrl, UICtrl){
 
-    let z = budgetCtrl.publicTest(10);
-
-    return {
-        anotherPublicTest: function(){
-            return z;
-        }
-    }
 
 })(budgetController, UIController); // variables passed here will be used as arguements 
