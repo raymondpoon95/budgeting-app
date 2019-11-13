c69dc664029973c3167f8cdaa7cef6bf9a6b5e1a
Raymond Poon <raymondpoon95@hotmail.co.uk>
2019-11-13 03:35:20 +0000

    added grab input function

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 /************************************************************
  * budget control module, handles the data
  */
-let budgetController = (function(){
+const budgetController = (function(){
 
 
 })();
@@ -15,8 +15,28 @@ let budgetController = (function(){
 /************************************************************
  * user interface module, handles the UI functions
  */
-let UIController = (function(){
-    // some code
+const UIController = (function(){
+    // private object DOMStrings so strings are not repeated and for ease
+    const DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+    }
+
+    // grab input
+    return {
+        getInput: function(){
+            return {
+                type: document.querySelector(DOMStrings.inputType).value, // will either be type inc or exp
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value,
+            };
+        },
+        getDOMStrings: function(){ // exposing the DOMStrings to the public 
+            return DOMStrings;
+        }  
+    }
 })();
 
 
@@ -25,7 +45,7 @@ let UIController = (function(){
  * controller module, handles the link between the UI and budget controller modules
  * the controller module always tells the other controllers what to do
  */
-let controller = (function(budgetCtrl, UICtrl){
+const controller = (function(budgetCtrl, UICtrl){
 
 
 })(budgetController, UIController); // variables passed here will be used as arguements 
